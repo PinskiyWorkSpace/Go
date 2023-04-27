21cb3e2d127763a2b15fba85d35aa808bb0e010a
Filipp Pinskiy <pinskiy.fs@ya.ru>
2023-04-27 19:53:42 +0300

    Выполнено задание

--- script/main.js
+++ script/main.js
@@ -5,3 +5,16 @@ import './modules/modal.js';
 acordeon();
 
 
+new Swiper('.swiper', {
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
